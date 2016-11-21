078d13cd8443c2e65d88bd6e943e886fcdc8ccca
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-11-21 11:52:28 +0000

    Added Javadoc to UserServiceInitializer

--- user-service/src/main/java/com/dev/ops/micro/service/user/UserServiceInitializer.java
+++ user-service/src/main/java/com/dev/ops/micro/service/user/UserServiceInitializer.java
@@ -1,13 +1,38 @@
+/*
+ *
+ * Copyright (C) 2016 Krishna Kuntala <kuntala.krishna@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.dev.ops.micro.service.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * The Class User Service Initializer is invoked to launch the REST service.
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dev.ops.micro.service.user"})
 public class UserServiceInitializer {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceInitializer.class, args);
 	}
